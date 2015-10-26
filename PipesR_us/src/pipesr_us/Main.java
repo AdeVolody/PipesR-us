08aa669f482f83efe387406bbc099e4ffb811788
Ade Crown <adecrown1@live.co.uk>
2015-10-26 12:07:20 +0000

    Main Files

--- PipesR_us/src/pipesr_us/Main.java
+++ PipesR_us/src/pipesr_us/Main.java
@@ -16,9 +16,38 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
         Pipes pier = new Type_5(1,1,3,1,true);
         
         System.out.println(pier.totalCost());
+        
+        
+        
+        
+        
+        
+       //Testing Basket and Orders Class
+                
+        // Creating new basket.
+        Basket basket1 = new Basket("Basket");
+
+        // Creating the orders.
+        Orders order1 = new Orders();
+        
+
+        // Creating the pipe.
+        Pipes type2 = new Type_2(1, 2, 2, 6, true);
+       
+        // Adding pipe to the orders.
+        order1.addPipe(type2);
+        
+
+        // Adding orders to the basket.
+        basket1.addOrder(order1);
+        
+
+        // Output information regarding the order.
+        basket1.ordersPlaced();
     }
     
 }
--- PipesR_us/src/pipesr_us/Orders.java
+++ PipesR_us/src/pipesr_us/Orders.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Adekunle Ogunade
+ * @author up720163
  */
 
 
